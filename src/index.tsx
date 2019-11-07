cc1926c316393c402478c9894254aa1d6127af70
Richard Muñoz <richard.munoz@crifa.com>
2019-11-07 17:19:20 -0500

    fixed index tsx to default imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+
+// TODO: define BrowserRouter or HashRouter
 import { HashRouter, Route, Switch } from "react-router-dom";
 
 // Styles
