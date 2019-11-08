57d38a1d342964ba4f439666433faae3b939fd43
Richard Muñoz <richard.munoz@crifa.com>
2019-11-08 16:24:47 -0500

    changed charts component to FC

--- src/views/Charts/Charts.tsx
+++ src/views/Charts/Charts.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Bar, Doughnut, Line, Pie, Polar, Radar } from "react-chartjs-2";
 import { CardColumns, Card, CardHeader, CardBody } from "reactstrap";
 
@@ -111,115 +111,113 @@ const polar = {
   labels: ["Red", "Green", "Yellow", "Grey", "Blue"]
 };
 
-class Charts extends React.Component {
-  render() {
-    return (
-      <div className="animated fadeIn">
-        <CardColumns className="cols-2">
-          <Card>
-            <CardHeader>
-              Line Chart
-              <div className="card-actions">
-                <a href="http://www.chartjs.org">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Line
-                  data={line}
-                  options={{
-                    maintainAspectRatio: false
-                  }}
-                />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Bar Chart
-              <div className="card-actions">
-                <a href="http://www.chartjs.org">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Bar
-                  data={bar}
-                  options={{
-                    maintainAspectRatio: false
-                  }}
-                />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Doughnut Chart
-              <div className="card-actions">
-                <a href="http://www.chartjs.org">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Doughnut data={doughnut} />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Radar Chart
-              <div className="card-actions">
-                <a href="http://www.chartjs.org">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Radar data={radar} />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Pie Chart
-              <div className="card-actions">
-                <a href="http://www.chartjs.org">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Pie data={pie} />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Polar Area Chart
-              <div className="card-actions">
-                <a href="http://www.chartjs.org">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Polar data={polar} />
-              </div>
-            </CardBody>
-          </Card>
-        </CardColumns>
-      </div>
-    );
-  }
-}
+const Charts: React.FC = () => {
+  return (
+    <div className="animated fadeIn">
+      <CardColumns className="cols-2">
+        <Card>
+          <CardHeader>
+            Line Chart
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Line
+                data={line}
+                options={{
+                  maintainAspectRatio: false
+                }}
+              />
+            </div>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            Bar Chart
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Bar
+                data={bar}
+                options={{
+                  maintainAspectRatio: false
+                }}
+              />
+            </div>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            Doughnut Chart
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Doughnut data={doughnut} />
+            </div>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            Radar Chart
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Radar data={radar} />
+            </div>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            Pie Chart
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Pie data={pie} />
+            </div>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader>
+            Polar Area Chart
+            <div className="card-actions">
+              <a href="http://www.chartjs.org">
+                <small className="text-muted">docs</small>
+              </a>
+            </div>
+          </CardHeader>
+          <CardBody>
+            <div className="chart-wrapper">
+              <Polar data={polar} />
+            </div>
+          </CardBody>
+        </Card>
+      </CardColumns>
+    </div>
+  );
+};
 
 export default Charts;
