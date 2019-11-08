de40e5ed11ffab248d0e006aba0e2e49d6486f17
Richard Muñoz <richard.munoz@crifa.com>
2019-11-08 12:51:32 -0500

    changed and fixed breadcrum component to FC

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,49 +1,54 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
-import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Breadcrumb as RsBreadcrum, BreadcrumbItem } from "reactstrap";
 import routes from "../../routes";
 
-const findRouteName = url => routes[url];
+const Breadcrumb: React.FC = props => {
+  const findRouteName = (url: string) => routes[url];
 
-const getPaths = pathname => {
-  const paths = ["/"];
+  const getPaths = (pathname: string) => {
+    const paths = ["/"];
 
-  if (pathname === "/") {
+    if (pathname === "/") {
+      return paths;
+    }
+
+    pathname.split("/").reduce((prev, curr, index) => {
+      const currPath = `${prev}/${curr}`;
+      paths.push(currPath);
+      return currPath;
+    });
     return paths;
-  }
+  };
 
-  pathname.split("/").reduce((prev, curr, index) => {
-    const currPath = `${prev}/${curr}`;
-    paths.push(currPath);
-    return currPath;
-  });
-  return paths;
+  const BreadcrumbsItem = ({ match, ...rest }) => {
+    const routeName = findRouteName(match.url);
+
+    if (routeName) {
+      return match.isExact ? (
+        <BreadcrumbItem active>{routeName}</BreadcrumbItem>
+      ) : (
+        <BreadcrumbItem>
+          <Link to={match.url || ""}>{routeName}</Link>
+        </BreadcrumbItem>
+      );
+    }
+    return null;
+  };
+
+  const Breadcrumbs = ({ location: { pathname }, match, ...rest }) => {
+    const paths = getPaths(pathname);
+    const items = paths.map((path, i) => (
+      <Route key={i++} path={path} component={BreadcrumbsItem} />
+    ));
+    return <RsBreadcrum>{items}</RsBreadcrum>;
+  };
+
+  return (
+    <div>
+      <Route path="/:path" component={Breadcrumbs} {...props} />
+    </div>
+  );
 };
 
-const BreadcrumbsItem = ({ match, ...rest }) => {
-  const routeName = findRouteName(match.url);
-  if (routeName) {
-    return match.isExact ? (
-      <BreadcrumbItem active>{routeName}</BreadcrumbItem>
-    ) : (
-      <BreadcrumbItem>
-        <Link to={match.url || ""}>{routeName}</Link>
-      </BreadcrumbItem>
-    );
-  }
-  return null;
-};
-
-const Breadcrumbs = ({ location: { pathname }, match, ...rest }) => {
-  const paths = getPaths(pathname);
-  const items = paths.map((path, i) => (
-    <Route key={i++} path={path} component={BreadcrumbsItem} />
-  ));
-  return <Breadcrumb>{items}</Breadcrumb>;
-};
-
-export default props => (
-  <div>
-    <Route path="/:path" component={Breadcrumbs} {...props} />
-  </div>
-);
+export default Breadcrumb;
