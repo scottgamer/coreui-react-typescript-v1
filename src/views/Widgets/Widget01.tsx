4e96577fb87aa8cec13c345cc1df9ac4a151d926
Richard Muñoz <richard.munoz@crifa.com>
2019-11-08 16:45:19 -0500

    fixed widgets and widget component

--- src/views/Widgets/Widget01.tsx
+++ src/views/Widgets/Widget01.tsx
@@ -1,57 +1,54 @@
-import * as React from 'react';
-import { Card, CardBody, Progress } from 'reactstrap';
-import classNames from 'classnames';
-import { mapToCssModules } from 'reactstrap/lib/utils';
+import React from "react";
+import { Card, CardBody, Progress } from "reactstrap";
+import classNames from "classnames";
+import { mapToCssModules } from "reactstrap/lib/utils";
 
-class Widget01 extends React.Component<any, any> {
-  render() {
-    const {
-      className,
-      cssModule,
-      header,
-      mainText,
-      smallText,
-      color,
-      value,
-      children,
-      variant,
-      ...attributes
-    } = this.props;
+const Widget01: React.FC<any> = props => {
+  const {
+    className,
+    cssModule,
+    header,
+    mainText,
+    smallText,
+    color,
+    value,
+    children,
+    variant,
+    ...attributes
+  } = props;
 
-    // demo purposes only
-    const progress = { style: '', color: color, value: value };
-    const card = { style: '', bgColor: '' };
+  // demo purposes only
+  const progress = { style: "", color, value };
+  const card = { style: "", bgColor: "" };
 
-    if (variant === 'inverse') {
-      progress.style = 'progress-white';
-      progress.color = '';
-      card.style = 'text-white';
-      card.bgColor = 'bg-' + color;
-    }
-
-    const classes = mapToCssModules(
-      classNames(className, card.style, card.bgColor),
-      cssModule,
-    );
-    progress.style = classNames('progress-xs my-3', progress.style);
-
-    return (
-      <Card className={classes} {...attributes}>
-        <CardBody>
-          <div className="h4 m-0">{header}</div>
-          <div>{mainText}</div>
-          <Progress
-            className={progress.style}
-            color={progress.color}
-            value={progress.value}
-          />
-          <small className="text-muted">{smallText}</small>
-          <div>{children}</div>
-        </CardBody>
-      </Card>
-    );
+  if (variant === "inverse") {
+    progress.style = "progress-white";
+    progress.color = "";
+    card.style = "text-white";
+    card.bgColor = "bg-" + color;
   }
-}
 
+  const classes = mapToCssModules(
+    classNames(className, card.style, card.bgColor),
+    cssModule
+  );
+  progress.style = classNames("progress-xs my-3", progress.style);
+
+  return (
+    <Card className={classes} {...attributes}>
+      <CardBody>
+        <div className="h4 m-0">{header}</div>
+        <div>{mainText}</div>
+        <Progress
+          className={progress.style}
+          color={progress.color}
+          value={progress.value}
+        />
+        <small className="text-muted">{smallText}</small>
+        <div>{children}</div>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default Widget01;
